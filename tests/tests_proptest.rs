b88826a5ffbf55a60b3e031ce35467575c065578
frozenlib <frozenlib@users.noreply.github.com>
2020-11-14 03:09:07 +0900

    Use test-strategy crate.

--- tests/tests_proptest.rs
+++ tests/tests_proptest.rs
@@ -1,143 +1,100 @@
-#[macro_use]
-extern crate proptest;
-
-extern crate gapbuf;
-
 use gapbuf::GapBuffer;
-
+use proptest::collection::vec;
 use proptest::prelude::*;
+use test_strategy::{proptest, Arbitrary};
 
-#[derive(Clone, Copy, Debug)]
+#[derive(Arbitrary, Clone, Copy, Debug)]
 enum VecAction {
-    Insert(usize),
-    Remove(usize),
-    SetGap(usize),
+    Insert(#[strategy(0..100usize)] usize),
+    Remove(#[strategy(0..100usize)] usize),
+    SetGap(#[strategy(0..100usize)] usize),
 }
 
-fn vec_actions_strategy() -> impl Strategy<Value = Vec<VecAction>> {
+#[proptest]
+fn insert_remove(#[strategy(vec(any::<VecAction>(), 0..100))] actions: Vec<VecAction>) {
     use self::VecAction::*;
-    let idx = 0..100usize;
-    let action = prop_oneof![
-        idx.clone().prop_map(Insert),
-        idx.clone().prop_map(Remove),
-        idx.clone().prop_map(SetGap),
-    ];
-    proptest::collection::vec(action, 0..100)
-}
 
-proptest! {
-    #[test]
-    fn insert_remove(actions in vec_actions_strategy()) {
-        use self::VecAction::*;
+    let mut e = Vec::new();
+    let mut a = GapBuffer::new();
 
-        let mut e = Vec::new();
-        let mut a = GapBuffer::new();
-
-        let mut value = 0;
-        for action in actions {
-            match action {
-                Insert(idx) => {
-                    value += 1;
-                    let idx = idx % (e.len() + 1);
-                    e.insert(idx, value);
-                    a.insert(idx, value);
-                }
-                Remove(idx) => {
-                    if !e.is_empty() {
-                        let idx = idx % e.len();
-                        e.remove(idx);
-                        a.remove(idx);
-                    }
-                }
-                SetGap(idx) => {
-                    let idx = idx % (e.len() + 1);
-                    a.set_gap(idx);
+    let mut value = 0;
+    for action in actions {
+        match action {
+            Insert(idx) => {
+                value += 1;
+                let idx = idx % (e.len() + 1);
+                e.insert(idx, value);
+                a.insert(idx, value);
+            }
+            Remove(idx) => {
+                if !e.is_empty() {
+                    let idx = idx % e.len();
+                    e.remove(idx);
+                    a.remove(idx);
                 }
             }
-            assert_eq!(a, e);
+            SetGap(idx) => {
+                let idx = idx % (e.len() + 1);
+                a.set_gap(idx);
+            }
         }
+        assert_eq!(a, e);
     }
 }
-#[derive(Debug)]
+#[derive(Arbitrary, Debug)]
 struct RangeArg {
+    #[strategy(0..10usize)]
     len: usize,
+    #[strategy(0..2usize)]
     reserve: usize,
+    #[strategy(0..=#len)]
     gap: usize,
+    #[strategy(0..=#len)]
     begin: usize,
+    #[strategy(#begin..=#len)]
     end: usize,
 }
-fn range_arg_strategy() -> impl Strategy<Value = RangeArg> {
-    (0..10usize)
-        .prop_flat_map(|len| (Just(len), 0..=len))
-        .prop_flat_map(|(len, begin)| (Just(len), Just(begin), begin..=len, 0..=len, 0..2usize))
-        .prop_map(|(len, begin, end, gap, reserve)| RangeArg {
-            len,
-            begin,
-            end,
-            gap,
-            reserve,
-        })
-}
-proptest! {
-    #[test]
-    fn range(a in range_arg_strategy()) {
-        let b: GapBuffer<_> = (0..a.len).collect();
-        let mut b = b.clone();
-        b.reserve_exact(a.reserve);
-        b.set_gap(a.gap);
+#[proptest]
+fn range(a: RangeArg) {
+    let b: GapBuffer<_> = (0..a.len).collect();
+    let mut b = b.clone();
+    b.reserve_exact(a.reserve);
+    b.set_gap(a.gap);
 
-        let e: Vec<_> = b
-            .iter()
-            .cloned()
-            .skip(a.begin)
-            .take(a.end - a.begin)
-            .collect();
-        prop_assert_eq!(b.range(a.begin..a.end), e);
-    }
+    let e: Vec<_> = b
+        .iter()
+        .cloned()
+        .skip(a.begin)
+        .take(a.end - a.begin)
+        .collect();
+    prop_assert_eq!(b.range(a.begin..a.end), e);
 }
 
-#[derive(Debug)]
+#[derive(Arbitrary, Debug)]
 struct SpliceArg {
+    #[strategy(0..10usize)]
     len: usize,
+    #[strategy(0..=#len)]
     begin: usize,
+    #[strategy(#begin..=#len)]
     end: usize,
+    #[strategy(0..=#len)]
     gap: usize,
+    #[strategy(0..2usize)]
     reserve: usize,
+    #[strategy(0..10usize)]
     len_insert: usize,
 }
-fn splice_arg_strategy() -> impl Strategy<Value = SpliceArg> {
-    (0..10usize)
-        .prop_flat_map(|len| (Just(len), 0..=len))
-        .prop_flat_map(|(len, begin)| {
-            (
-                Just(len),
-                Just(begin),
-                begin..=len,
-                0..=len,
-                0..2usize,
-                0..10usize,
-            )
-        }).prop_map(|(len, begin, end, gap, reserve, len_insert)| SpliceArg {
-            len,
-            begin,
-            end,
-            gap,
-            reserve,
-            len_insert,
-        })
-}
-proptest! {
-    #[test]
-    fn splice(a in splice_arg_strategy()) {
-        let mut e: Vec<_> = (0..a.len).collect();
-        let er: Vec<_> = e.splice(a.begin..a.end, 10..10 + a.len_insert).collect();
+#[proptest]
+fn splice(a: SpliceArg) {
+    let mut e: Vec<_> = (0..a.len).collect();
+    let er: Vec<_> = e.splice(a.begin..a.end, 10..10 + a.len_insert).collect();
 
-        let mut b: GapBuffer<_> = (0..a.len).collect();
-        b.reserve(a.reserve);
-        b.set_gap(a.gap);
+    let mut b: GapBuffer<_> = (0..a.len).collect();
+    b.reserve(a.reserve);
+    b.set_gap(a.gap);
 
-        let br: Vec<_> = b.splice(a.begin..a.end, 10..10 + a.len_insert).collect();
-        assert_eq!(br, er, "removed list");
-        assert_eq!(b, e, "new list");
-    }
+    let br: Vec<_> = b.splice(a.begin..a.end, 10..10 + a.len_insert).collect();
+    assert_eq!(br, er, "removed list");
+    assert_eq!(b, e, "new list");
 }
