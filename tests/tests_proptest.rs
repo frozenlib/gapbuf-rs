541417ffcf1c310b5b21b78a3f6c3a5dd4d28f39
frozenlib <frozenlib@users.noreply.github.com>
2020-11-14 03:28:04 +0900

    Use parameter attribute.

--- tests/tests_proptest.rs
+++ tests/tests_proptest.rs
@@ -1,6 +1,5 @@
 use gapbuf::GapBuffer;
-use proptest::collection::vec;
-use proptest::prelude::*;
+use proptest::{collection::vec, prelude::*};
 use test_strategy::{proptest, Arbitrary};
 
 #[derive(Arbitrary, Clone, Copy, Debug)]
@@ -41,60 +40,40 @@ fn insert_remove(#[strategy(vec(any::<VecAction>(), 0..100))] actions: Vec<VecAc
         assert_eq!(a, e);
     }
 }
-#[derive(Arbitrary, Debug)]
-struct RangeArg {
-    #[strategy(0..10usize)]
-    len: usize,
-    #[strategy(0..2usize)]
-    reserve: usize,
-    #[strategy(0..=#len)]
-    gap: usize,
-    #[strategy(0..=#len)]
-    begin: usize,
-    #[strategy(#begin..=#len)]
-    end: usize,
-}
 #[proptest]
-fn range(a: RangeArg) {
-    let b: GapBuffer<_> = (0..a.len).collect();
+fn range(
+    #[strategy(0..10usize)] len: usize,
+    #[strategy(0..2usize)] reserve: usize,
+    #[strategy(0..=#len)] gap: usize,
+    #[strategy(0..=#len)] begin: usize,
+    #[strategy(#begin..=#len)] end: usize,
+) {
+    let b: GapBuffer<_> = (0..len).collect();
     let mut b = b.clone();
-    b.reserve_exact(a.reserve);
-    b.set_gap(a.gap);
+    b.reserve_exact(reserve);
+    b.set_gap(gap);
 
-    let e: Vec<_> = b
-        .iter()
-        .cloned()
-        .skip(a.begin)
-        .take(a.end - a.begin)
-        .collect();
-    prop_assert_eq!(b.range(a.begin..a.end), e);
+    let e: Vec<_> = b.iter().cloned().skip(begin).take(end - begin).collect();
+    prop_assert_eq!(b.range(begin..end), e);
 }
 
-#[derive(Arbitrary, Debug)]
-struct SpliceArg {
-    #[strategy(0..10usize)]
-    len: usize,
-    #[strategy(0..=#len)]
-    begin: usize,
-    #[strategy(#begin..=#len)]
-    end: usize,
-    #[strategy(0..=#len)]
-    gap: usize,
-    #[strategy(0..2usize)]
-    reserve: usize,
-    #[strategy(0..10usize)]
-    len_insert: usize,
-}
 #[proptest]
-fn splice(a: SpliceArg) {
-    let mut e: Vec<_> = (0..a.len).collect();
-    let er: Vec<_> = e.splice(a.begin..a.end, 10..10 + a.len_insert).collect();
+fn splice(
+    #[strategy(0..10usize)] len: usize,
+    #[strategy(0..=#len)] begin: usize,
+    #[strategy(#begin..=#len)] end: usize,
+    #[strategy(0..=#len)] gap: usize,
+    #[strategy(0..2usize)] reserve: usize,
+    #[strategy(0..10usize)] len_insert: usize,
+) {
+    let mut e: Vec<_> = (0..len).collect();
+    let er: Vec<_> = e.splice(begin..end, 10..10 + len_insert).collect();
 
-    let mut b: GapBuffer<_> = (0..a.len).collect();
-    b.reserve(a.reserve);
-    b.set_gap(a.gap);
+    let mut b: GapBuffer<_> = (0..len).collect();
+    b.reserve(reserve);
+    b.set_gap(gap);
 
-    let br: Vec<_> = b.splice(a.begin..a.end, 10..10 + a.len_insert).collect();
+    let br: Vec<_> = b.splice(begin..end, 10..10 + len_insert).collect();
     assert_eq!(br, er, "removed list");
     assert_eq!(b, e, "new list");
 }
